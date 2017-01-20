25a6e688e9a30133453a6eb39a8e61559db74628
njdaniel <nicholasjdaniel@gmail.com>
2017-01-20 14:15:54 -0500

    fixed search_all error but getting dupes now

--- accounts/views.py
+++ accounts/views.py
@@ -7,7 +7,16 @@ from accounts.models import Account
 def index(req):
     if req.GET:
         search_term = req.GET['S']
-        results = Account.objects.filter(alias__contains=search_term)
+        # TODO: Search all fields in Account
+        results = Account.objects.filter(alias__icontains=search_term), \
+                  Account.objects.filter(mailbox__icontains=search_term), \
+                  Account.objects.filter(type__icontains=search_term), \
+                  Account.objects.filter(whitelist_ip__icontains=search_term), \
+                  Account.objects.filter(inbox__icontains=search_term), \
+                  Account.objects.filter(outbox__icontains=search_term), \
+                  Account.objects.filter(host_url__icontains=search_term), \
+                  Account.objects.filter(server__icontains=search_term), \
+                  Account.objects.filter(customer__icontains=search_term),
         return render_to_response('accounts/index.html', {'results': results})
     return render_to_response('accounts/index.html', {})
 
